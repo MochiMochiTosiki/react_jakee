a7f1307d34f2dc7b6649455bfac2ff85e85dcd69
toshiki <mochi.mochi.0397@gmail.com>
2021-12-08 18:54:48 +0900

    3-1 (DOMの削除)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-console.log("test");
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "ボタン";
+const divEl = document.querySelector(".container");
+divEl.appendChild(buttonEl);
 
-const title1 = document.getElementById("title");
-console.log(title1);
+const h1El = document.getElementById("title");
+const bodyEl = document.querySelector("body");
+bodyEl.removeChild(h1El);
 
-const title2 = document.querySelector("#title");
-console.log(title2);
-
-const containers = document.getElementsByClassName("container");
-console.log(containers);
-
-const container = document.querySelector(".container");
-console.log(container);
-
-const nushidaEL = document.createElement("nushida");
-console.log(nushidaEL);
-
-const divEl = document.createElement("div");
-const pEl = document.createElement("p");
-const h2El = document.createElement("h2");
-divEl.appendChild(pEl);
-divEl.appendChild(h2El);
-divEl.prepend(pEl);
-divEl.prepend(h2El);
-console.log(divEl);
+const bodyEl = document.querySelector("body");
+bodyEl.textContent = null;
