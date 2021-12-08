3b131d9dcbddcaa591d1370707f8b932243e487e
toshiki <mochi.mochi.0397@gmail.com>
2021-12-08 16:31:53 +0900

    3-2 (DOMの作成、追加)

--- src/index.js
+++ src/index.js
@@ -11,3 +11,15 @@ console.log(containers);
 
 const container = document.querySelector(".container");
 console.log(container);
+
+const nushidaEL = document.createElement("nushida");
+console.log(nushidaEL);
+
+const divEl = document.createElement("div");
+const pEl = document.createElement("p");
+const h2El = document.createElement("h2");
+divEl.appendChild(pEl);
+divEl.appendChild(h2El);
+divEl.prepend(pEl);
+divEl.prepend(h2El);
+console.log(divEl);
