54d5a1b69e0106a78cc3a78e1af3aa92e4a068e0
toshiki <mochi.mochi.0397@gmail.com>
2021-12-11 01:23:28 +0900

    8-6 省略可能な型の定義

--- chapter8/src/components/ListItem.tsx
+++ chapter8/src/components/ListItem.tsx
@@ -1,11 +1,15 @@
 import type { FC } from "react";
 import type { User } from "../types/user";
 
-export const ListItem: FC<User> = (props: User) => {
+export const ListItem: FC<User> = props => {
     const { id, name, age, personalColor } = props;
     return (
         <p style={{color: personalColor }}>
             {id} : {name}({age})
         </p>
     )
+}
+
+ListItem.defaultProps = {
+    personalColor: "red"
 }
